79c053f9316ce90fde4fda3e1db111d9a3dfa90c
chachakawooka <chachakawooka@googlemail.com>
2019-07-03 21:55:38 +0100

    Updates

---
+++ precache-manifest.a5d00b466aa7f278b97b1a69f499acb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b0f3f500def03c9a9148ef90892014",
+    "url": "/credits-slots/index.html"
+  },
+  {
+    "revision": "0b47836ec152280e75a0",
+    "url": "/credits-slots/static/css/main.b8cd683c.chunk.css"
+  },
+  {
+    "revision": "4a483378f10ff1edb951",
+    "url": "/credits-slots/static/js/2.4552cfa9.chunk.js"
+  },
+  {
+    "revision": "0b47836ec152280e75a0",
+    "url": "/credits-slots/static/js/main.a6fe7a68.chunk.js"
+  },
+  {
+    "revision": "a052351442091cc0fd60",
+    "url": "/credits-slots/static/js/runtime~main.ce8f83cb.js"
+  }
+]);
